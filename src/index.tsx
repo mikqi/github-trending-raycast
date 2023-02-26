e55d3fb1d11138531c229a879887abf28c9f4d4b
Muhammad Rivki <Muhammad.Rivki@live.com>
2023-02-26 20:26:01 +0700

    feat: change state to useReducer

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import { ActionPanel, Action, List, Cache } from '@raycast/api'
 import { useFetch, Response } from '@raycast/utils'
-import { useState, useEffect } from 'react'
+import { useEffect, useReducer } from 'react'
 import trending from 'trending-github'
 
 import { PROGRAMMING_LANGUAGES, DATE_RANGE_OPTIONS } from './constants'
@@ -56,78 +56,127 @@ const LanguageActions = ({ repo, onChangeRange }: { repo: RepoType; onChangeRang
   )
 }
 
+type CommandState = {
+  selectedLanguage: string
+  isLoading: boolean
+  repos: RepoType[]
+  query: string
+  range: string
+}
+
+type CommandAction =
+  | { type: 'SET_SELECTED_LANGUAGE'; payload: string }
+  | { type: 'SET_IS_LOADING'; payload: boolean }
+  | { type: 'SET_REPOS'; payload: RepoType[] }
+  | { type: 'SET_QUERY'; payload: string }
+  | { type: 'SET_RANGE'; payload: string }
+
+const initialState: CommandState = {
+  selectedLanguage: '',
+  isLoading: false,
+  repos: [],
+  query: '',
+  range: 'daily',
+}
+
+const commandReducer = (state: CommandState, action: CommandAction) => {
+  switch (action.type) {
+    case 'SET_SELECTED_LANGUAGE':
+      return { ...state, selectedLanguage: action.payload }
+    case 'SET_IS_LOADING':
+      return { ...state, isLoading: action.payload }
+    case 'SET_REPOS':
+      return { ...state, repos: action.payload }
+    case 'SET_QUERY':
+      return { ...state, query: action.payload }
+    case 'SET_RANGE':
+      return { ...state, range: action.payload }
+    default:
+      return state
+  }
+}
+
 export default function Command() {
-  const [selectedLanguage, setSelectedLanguage] = useState('')
-  const [isLoading, setIsLoading] = useState(false)
-  const [repos, setRepos] = useState<RepoType[]>([])
-  const [query, setQuery] = useState('')
-  const [range, setRange] = useState('daily')
+  const [state, dispatch] = useReducer(commandReducer, initialState)
 
   useEffect(() => {
     async function fetchRepos() {
       try {
-        setIsLoading(true)
-        const result = await trending(range, selectedLanguage)
-        setRepos(result as RepoType[])
-        setIsLoading(false)
+        dispatch({ type: 'SET_IS_LOADING', payload: true })
+        const result = await trending(state.range, state.selectedLanguage)
+        dispatch({ type: 'SET_REPOS', payload: result as RepoType[] })
+        dispatch({ type: 'SET_IS_LOADING', payload: false })
       } catch (error) {
-        setIsLoading(false)
+        dispatch({ type: 'SET_IS_LOADING', payload: false })
       }
     }
 
     fetchRepos()
-  }, [selectedLanguage, range])
+  }, [state.selectedLanguage, state.range])
 
-  const handleSearchTextChange = (searchText: string) => {
-    if (searchText === '') {
-      return setSelectedLanguage('')
+  const handleSearchFilterChange = (searchFilter: string) => {
+    if (searchFilter === '') {
+      return dispatch({ type: 'SET_SELECTED_LANGUAGE', payload: '' })
     }
-    setQuery(searchText)
+    dispatch({ type: 'SET_QUERY', payload: searchFilter })
   }
 
-  const handleRangeChange = (range: string) => {
-    setRange(range)
+  const handleTimeRangeChange = (timeRange: string) => {
+    dispatch({ type: 'SET_RANGE', payload: timeRange })
   }
 
   return (
     <List
-      onSearchTextChange={handleSearchTextChange}
+      onSearchTextChange={handleSearchFilterChange}
       navigationTitle="Trending Repositories"
-      isLoading={repos.length === 0 || isLoading}
+      isLoading={state.isLoading || state.repos.length === 0}
       searchBarPlaceholder="Filter repos by name..."
-      searchBarAccessory={<RangeDropdown selectedRange={range} onChangeRange={handleRangeChange} />}
+      searchBarAccessory={<RangeDropdown selectedRange={state.range} onChangeRange={handleTimeRangeChange} />}
       throttle
     >
-      {query === ''
-        ? repos.map((repo) => (
-            <List.Item
-              key={repo.author + '/' + repo.name}
-              title={repo.author + '/' + repo.name}
-              subtitle={{
-                value: repo.language,
-                tooltip: repo.description,
-              }}
-              accessories={[{ text: `${repo.stars} ☆` }]}
-              actions={<LanguageActions repo={repo} onChangeRange={handleRangeChange} />}
-            />
+      {state.query === ''
+        ? state.repos.map((repo) => (
+            <ListItem key={repo.author + '/' + repo.name} repo={repo} onRangeChange={handleTimeRangeChange} />
           ))
-        : PROGRAMMING_LANGUAGES.filter((lang) => lang.toLowerCase().includes(query.toLowerCase())).map((lang, idx) => (
-            <List.Item
-              key={lang + idx}
-              title={lang}
-              actions={
-                <ActionPanel>
-                  <Action
-                    title="Select Language"
-                    onAction={() => {
-                      setSelectedLanguage(lang)
-                      setQuery('')
-                    }}
-                  />
-                </ActionPanel>
-              }
-            />
-          ))}
+        : PROGRAMMING_LANGUAGES.filter((lang) => lang.toLowerCase().includes(state.query.toLowerCase())).map(
+            (lang, idx) => (
+              <List.Item
+                key={lang + idx}
+                title={lang}
+                actions={
+                  <ActionPanel>
+                    <Action
+                      title="Select Language"
+                      onAction={() => {
+                        dispatch({
+                          type: 'SET_SELECTED_LANGUAGE',
+                          payload: lang,
+                        })
+                        dispatch({ type: 'SET_QUERY', payload: '' })
+                      }}
+                    />
+                  </ActionPanel>
+                }
+              />
+            ),
+          )}
     </List>
   )
 }
+
+type ListItemProps = {
+  repo: RepoType
+  onRangeChange: (range: string) => void
+}
+
+const ListItem = ({ repo, onRangeChange }: ListItemProps) => (
+  <List.Item
+    title={repo.author + '/' + repo.name}
+    subtitle={{
+      value: repo.language,
+      tooltip: repo.description,
+    }}
+    accessories={[{ text: `${repo.stars} ☆` }]}
+    actions={<LanguageActions repo={repo} onChangeRange={onRangeChange} />}
+  />
+)
